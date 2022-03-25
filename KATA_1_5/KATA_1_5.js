6b9366dd54fd3b66aae0cfa7ea1661c1c0ac6513
Grigoriy Basov <90413874+GrishaBasov@users.noreply.github.com>
2022-03-25 22:57:29 +0300

    Правки_2 от 25.03.22

--- KATA_1_5/KATA_1_5.js
+++ KATA_1_5/KATA_1_5.js
@@ -12,6 +12,33 @@ const swiper = new Swiper('.swiper', {
     },
 });
 
+
+// const swiper = new Swiper('.swiper', {
+//     // Optional parameters
+//     direction: 'horizontal',
+//     loop: false,
+//     slidesPerView: "auto",
+//
+//     // If we need pagination
+//     pagination: {
+//         el: '.swiper-pagination',
+//     },
+//
+//     // Navigation arrows
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//     },
+//
+//     // And if we need scrollbar
+//     scrollbar: {
+//         el: '.swiper-scrollbar',
+//     },
+// });
+
+
+
+
 let button = document.querySelector('.read-more')
 let menuOpen = document.querySelector('.main-menu')
 let menuClose = document.querySelector('.hide')
